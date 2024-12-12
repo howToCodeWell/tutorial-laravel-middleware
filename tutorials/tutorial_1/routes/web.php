7b13f3e61178c8a377abc940f690fa016e38a93c
pfwd <hello@websomatic.co.uk>
2024-12-12 14:51:08 +0000

    Removing blank line

--- tutorials/tutorial_1/routes/web.php
+++ tutorials/tutorial_1/routes/web.php
@@ -3,21 +3,6 @@
 use App\Http\Middleware\EnsureKeyIsValid;
 use Illuminate\Support\Facades\Route;
 
-// Adds a middleware to route '/'
-// Route::get('/', function () {
-//     return view('welcome');
-// })->withMiddleware([EnsureKeyIsValid::class]);
-
-// Route::middleware([EnsureKeyIsValid::class])->group(function () {
-//     Route::get('/', function () {
-//         return view('welcome');
-//     });
- 
-//     Route::get('/login', function () {
-//         return view('login');
-//     })->withoutMiddleware([EnsureKeyIsValid::class]);
-// });
-
 Route::middleware([EnsureKeyIsValid::class])->group(function () {
     Route::get('/', function () {
         return view('welcome');
